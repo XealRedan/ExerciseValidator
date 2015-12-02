912dd1ae775183edf7346b14d9bb7e1bbf04b82c
Alexandre Lombard <fitz4591@gmail.com>
2015-12-02 21:40:36 +0100

    Modified code snippet of controller

--- src/main/java/com/exercisevalidator/ExerciseValidatorController.java
+++ src/main/java/com/exercisevalidator/ExerciseValidatorController.java
@@ -24,6 +24,9 @@ package com.exercisevalidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Controller for exercise validator
@@ -36,8 +39,9 @@ public class ExerciseValidatorController {
     }
 
     @RequestMapping(value = "/Validate", method = RequestMethod.POST)
-    public String showExerciseValidation() {
-        // TODO
+    public @ResponseBody String upload(
+            @RequestParam("file") MultipartFile file) {
+
         return "Index";
     }
 }
